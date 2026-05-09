6cdd45ee113ee6c5498052b6b4dfc05e11fdc4e9
sebastianschlatter <sebastian.schlatter@web.de>
2026-05-09 19:43:06 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
         scope: '/',
         start_url: '/',
         icons: [
-          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'alfer logo neu 192x192px.png', sizes: '192x192', type: 'image/png' },
           { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
         ]
       },
